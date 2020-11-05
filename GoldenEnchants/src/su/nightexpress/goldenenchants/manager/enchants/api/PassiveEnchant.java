b2cf804f816f7b62d2ad151fef90ea80f0c9c71e
nulli0n <glitchy.cat@ya.ru>
2020-11-05 12:46:56 +0500

    Release

--- GoldenEnchants/src/su/nightexpress/goldenenchants/manager/enchants/api/PassiveEnchant.java
+++
@@ -1,9 +0,0 @@
-package su.nightexpress.goldenenchants.manager.enchants.api;
-
-import org.bukkit.entity.LivingEntity;
-import org.jetbrains.annotations.NotNull;
-
-public interface PassiveEnchant {
-	
-	void use(@NotNull LivingEntity user, int lvl);
-}
